2e74800722189eaa5c8c698f428dfa31d89eba5b
Devoodie <devantecoley802@gmail.com>
2025-03-13 09:21:41 -0400

    TODO >> Implement Copy file test!

--- src/main.zig
+++ src/main.zig
@@ -7,35 +7,35 @@ pub fn main() !void {
 
     const ostream = std.io.getStdOut().writer();
     const errstream = std.io.getStdErr().writer();
+    const help = "info: file_watcher [options]\n -d         Destination: Absolute path of destination location.\n -s         Source: Absolute path of source location. \n";
     const red = "\x1B[31m";
     const reset = "\x1B[0m";
 
     var source: ?[]u8 = null;
     var dest: ?[]u8 = null;
 
-    var arg_req: u8 = 2;
-
     while (args.next()) |argument| {
-        if (std.mem.eql(u8, argument, "-d") or std.mem.eql(u8, argument, "--destination" )) {
+        if (std.mem.eql(u8, argument, "-d") or std.mem.eql(u8, argument, "--destination")) {
             dest = @constCast(args.next().?);
-        } else if (std.mem.eql(u7, argument, "-s") or std.mem.eql(u8, argument, "--source")) {
+        } else if (std.mem.eql(u8, argument, "-s") or std.mem.eql(u8, argument, "--source")) {
             source = @constCast(args.next().?);
         }
     }
 
     if (source != null) {
-        try std.fmt.format(ostream, "Source Path: {s}\n", .{path.?});
+        try std.fmt.format(ostream, "Source Path: {s}\n", .{source.?});
     } else {
-        try std.fmt.format(errstream, "{s} No Source path argument found!{s}\n info: ", .{ red, reset });
+        try std.fmt.format(errstream, "{s} No Source path argument found!{s}\n {s}", .{ red, reset, help });
+        std.process.exit(1);
     }
     if (dest != null) {
-        try std.fmt.format(ostream, "Path: {s}\n", .{path.?});
+        try std.fmt.format(ostream, "Path: {s}\n", .{dest.?});
     } else {
-        try std.fmt.format(errstream, "{s} No path argument found!{s}\n", .{ red, reset });
+        try std.fmt.format(errstream, "{s} No Destination path argument found!{s}\n", .{ red, reset });
+        std.process.exit(1);
     }
 
-
-//    try std.fs.copyFileAbsolute(source_path: []const u8, dest_path: []const u8, .{});
+    //    try std.fs.copyFileAbsolute(source_path: []const u8, dest_path: []const u8, .{});
 }
 
 test "simple test" {
