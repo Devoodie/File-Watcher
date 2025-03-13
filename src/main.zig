a9c7c6dca7f2d98ba6ede6ad87ad21fe07865196
Devante Coley <devantecoley802@gmail.com>
2025-03-13 08:51:49 -0400

    TODO >> -m test copygying directories!

--- src/main.zig
+++ src/main.zig
@@ -2,23 +2,40 @@ const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
     var args = std.process.args();
-    var ostream = std.io.getStdOut().writer();
-    var errstream = std.io.getStdErr().writer();
 
-    var path: ?[]u8 = undefined;
+    const ostream = std.io.getStdOut().writer();
+    const errstream = std.io.getStdErr().writer();
+    const red = "\x1B[31m";
+    const reset = "\x1B[0m";
+
+    var source: ?[]u8 = null;
+    var dest: ?[]u8 = null;
+
+    var arg_req: u8 = 2;
+
     while (args.next()) |argument| {
-        if (std.mem.eql(u8, argument, "-p")) {
-            path = @constCast(args.next().?);
+        if (std.mem.eql(u8, argument, "-d") or std.mem.eql(u8, argument, "--destination" )) {
+            dest = @constCast(args.next().?);
+        } else if (std.mem.eql(u7, argument, "-s") or std.mem.eql(u8, argument, "--source")) {
+            source = @constCast(args.next().?);
         }
     }
-    if (path != null) {
-        std.debug.debug("{s}", .{path.?});
+
+    if (source != null) {
+        try std.fmt.format(ostream, "Source Path: {s}\n", .{path.?});
     } else {
-        errstream.write("No Path variable found ");
+        try std.fmt.format(errstream, "{s} No Source path argument found!{s}\n info: ", .{ red, reset });
     }
+    if (dest != null) {
+        try std.fmt.format(ostream, "Path: {s}\n", .{path.?});
+    } else {
+        try std.fmt.format(errstream, "{s} No path argument found!{s}\n", .{ red, reset });
+    }
+
+
+//    try std.fs.copyFileAbsolute(source_path: []const u8, dest_path: []const u8, .{});
 }
 
 test "simple test" {
