bbb52958dd8620eb9884d236f8d4d6615d1ed290
Devante Coley <devantecoley802@gmail.com>
2025-03-14 01:01:45 -0400

    TODO >> Handle Recursive Directories!

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const red = "\x1B[31m";
+const reset = "\x1B[0m";
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -8,8 +10,6 @@ pub fn main() !void {
     const ostream = std.io.getStdOut().writer();
     const errstream = std.io.getStdErr().writer();
     const help = "info: file_watcher [options]\n -d         Destination: Absolute path of destination location.\n -s         Source: Absolute path of source location. \n";
-    const red = "\x1B[31m";
-    const reset = "\x1B[0m";
 
     var source: ?[]u8 = null;
     var dest: ?[]u8 = null;
@@ -35,20 +35,63 @@ pub fn main() !void {
         std.process.exit(1);
     }
 
-    const source_dir = try std.fs.openDirAbsolute(source.?, .{});
+    var source_dir = try std.fs.openDirAbsolute(source.?, .{ .iterate = true });
     defer source_dir.close();
 
-    const dest_dir = try std.fs.openDirAbsolute(dest.?, .{});
+    var dest_dir = try std.fs.openDirAbsolute(dest.?, .{ .iterate = true });
     defer dest_dir.close();
 
-    var src_meta: std.fs.File.Metadata = undefined;
-    var dest_meta: std.fs.File.Metadata = undefined;
+    //    var src_meta: std.fs.File.Metadata = undefined;
+    //   var dest_meta: std.fs.File.Metadata = undefined;
 
-    while (true) {
-        src_meta = try source_dir.metadata();
-        dest_meta = try dest_dir.metadata();
+    //while (true) {
+    // src_meta = try source_dir.metadata();
+    //    dest_meta = try dest_dir.metadata();
 
-        if (src_meta.modified() < dest_meta.modified()) {}
+    //   if (src_meta.modified() < dest_meta.modified()) {}
+    // }
+    try copyFiles(&source_dir, &dest_dir, ostream, errstream);
+}
+
+pub fn copyFiles(source: *std.fs.Dir, dest: *std.fs.Dir, ostream: std.fs.File.Writer, errstream: std.fs.File.Writer) !void {
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    const allocator = gpa.allocator();
+
+    _ = ostream;
+
+    var iteators = source.iterate();
+
+    while (try iteators.next()) |file| {
+        const file_name = file.name;
+
+        if (file.kind == .directory) {
+            const dest_dir: std.fs.Dir = blk: {
+                //open the first level of files in destination and see if it exists
+                //if not create it
+                if (dest.openDir(file_name, .{ .iterate = true })) |dir| {
+                    break :blk dir;
+                } else |err| switch (err) {
+                    std.fs.Dir.OpenError.FileNotFound => {
+                        try std.fmt.format(errstream, "{s}No file Found in Destination Directory: {s}\nCreating New File {s}{s}\n", .{ red, file_name, file_name, reset });
+                        std.log.err("{s}No file Found in Destination Directory: {s}\nCreating New File {s}{s}\n", .{ red, file_name, file_name, reset });
+                        break :blk try dest.makeOpenPath(file_name, .{});
+                    },
+                    else => {
+                        return err;
+                    },
+                }
+            };
+            const sub_dir = try source.openDir(file_name, .{ .iterate = true });
+
+            var walker = try sub_dir.walk(allocator);
+            while (try walker.next()) |sub_file| {
+                if (sub_file.kind == .directory) {
+                    _ = void;
+                } else {
+                    try sub_dir.copyFile(sub_file.basename, dest_dir, "./", .{});
+                }
+            }
+        }
     }
 }
 
